990c62a9e8c064355ed4f18fdafb31c9f036fa1d
Septiamalia <amaliasepti968@gmail.com>
2026-04-25 18:25:57 +0700

    backend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -8,22 +9,41 @@ app.use(cors());
 let kondisi = "Basah";
 let pump = "OFF";
 
-app.get("/", (req, res) => {
-  res.send("Backend jalan!");
-});
+const TOKEN = "8771973466:AAFhS4_Lh3BQI_bfyk2-yivd0BMP818dr_8";
+const CHAT_ID = "1868833642";
+
+function kirimTelegram(msg) {
+  axios.post(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    chat_id: CHAT_ID,
+    text: msg
+  });
+}
+
+app.get("/", (req, res) => res.send("Backend jalan"));
 
 app.get("/status", (req, res) => {
   res.json({ kondisi, pump });
 });
 
+app.post("/sensor", (req, res) => {
+  kondisi = req.body.kondisi;
+
+  if (kondisi === "Kering") {
+    kirimTelegram("⚠️ Tanah kering!");
+  }
+
+  res.send("OK");
+});
+
 app.post("/siram", (req, res) => {
   pump = "ON";
-  res.send("Pompa nyala");
+  kirimTelegram("💧 Disiram dari web");
+  res.send("ON");
 });
 
 app.get("/pump", (req, res) => {
   res.json({ pump });
   pump = "OFF";
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server jalan di port " + PORT));
+
+app.listen(3000, () => console.log("Server jalan di 3000"));
